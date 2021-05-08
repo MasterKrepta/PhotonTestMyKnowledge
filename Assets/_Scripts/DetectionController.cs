39f287105705b0638e0bac69c23f573ef0a35638
Brian <brianj.pajak@gmail.com>
2021-05-07 20:40:59 -0400

    Detection Changes

--- Assets/_Scripts/DetectionController.cs
+++ Assets/_Scripts/DetectionController.cs
@@ -7,16 +7,19 @@ public class DetectionController : MonoBehaviour
 {
     [SerializeField] float DetectionRange = 10;
     [SerializeField] float coneRadius = 0.5f;
-    [SerializeField] float DetectTime = 1.5f;
+    [SerializeField] float DetectTime = 1.5f, DetectionCoolDown;
     [SerializeField] GameObject detectionSphere;
     [SerializeField] Vector3 detectionLoc;
 
+    bool isDetected = false;
+    float timeInSight = 0;
     SphereCollider childSphere;
     // Start is called before the first frame update
     void Start()
     {
         detectionSphere.transform.position = detectionLoc;
         childSphere = detectionSphere.GetComponent<SphereCollider>();
+        childSphere.enabled = true;
     }
 
     // Update is called once per frame
@@ -34,6 +37,31 @@ public class DetectionController : MonoBehaviour
         //CheckForHit(rgt);
         //CheckForHit(lft);
 
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (isDetected == true)
+            return;
+
+        var target = other.GetComponent<DetectionTarget>();
+        if (target != null)
+        {
+            
+            timeInSight += Time.deltaTime;
+            if (timeInSight >= DetectTime)
+            {
+                Debug.Log("Detected");
+                isDetected = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        timeInSight = 0;
+        isDetected = false;
+        DebugConsole.Instance.Log("Reset");
     }
 
     private void OnTriggerEnter(Collider other)
